b8a213c613126afe034881e06d22d04ada826db9
tstohn <tim.stohn@yahoo.com>
2026-03-26 00:07:06 +0100

    CMAKE

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,14 +1,23 @@
 Write-Host "=== LoCo Bootstrap ==="
 
+# Clone vcpkg if missing
 if (!(Test-Path "vcpkg")) {
     git clone https://github.com/microsoft/vcpkg.git
-    cd vcpkg
-    .\bootstrap-vcpkg.bat
-    cd ..
 }
 
-cmake --preset release
-cmake --build --preset release
+cd vcpkg
+
+# Make sure we have the full history
+git fetch --unshallow
+git fetch --all
+
+# Bootstrap vcpkg (disable telemetry)
+.\bootstrap-vcpkg.bat --disableMetrics
+cd ..
+
+# Configure & build with Visual Studio generator
+cmake -S . -B build -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE=.\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release
 
 Write-Host "=== DONE ==="
-Write-Host "Binary: .\bin\loco.exe"
+Write-Host "Binary: .\build\Release\loco.exe"
