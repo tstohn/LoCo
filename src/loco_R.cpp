134df8a9eef4a97efe2a44bbc404e07da2f258aa
tstohn <tim.stohn@yahoo.com>
2026-06-27 10:45:03 +0200

    add also warning for single neighbourhood to R package

--- src/loco_R.cpp
+++ src/loco_R.cpp
@@ -241,6 +241,13 @@ Rcpp::List run_correlation_propagation_across_graph(const SingleCellData& inFile
     }
     LOCO_OUT << "STEP 1\n";
     LOCO_OUT << "\tCreating " << numberNeighbourhoodsCalculated << " neighbourhoods with " << neighborhoodSize << " cells\n";
+
+    if(numberNeighbourhoodsCalculated == 1)
+    {
+        std::cout << "WARNING: YOU ARE RUNNING LoCo WITH A SINGLE NEIGHBOURHOOD!!!\n";
+        std::cout << " LoCo still estimated correlations within this neihgbourhood but can not detect smooth changes\n";
+    }
+
     bool printStatusUpdateCellDistCalc = true;
     unsigned int scGraphKnn = neighborhoodSize; //the KNN value is the number of cells in a neighborhood, we ONLY have to calcualte the knn closest neighbors, no need for more
     bool precalculateAllDistances = false;
